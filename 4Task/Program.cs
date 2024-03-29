d69c60001be03c1bcf4a7cc7526fd741d5d16d0c
Nozim Yokubov <anorcomander@gmail.com>
2024-03-29 22:58:01 +0500

    add lesson3:4Task

---
+++ 4Task/Program.cs
@@ -0,0 +1,41 @@
+﻿void FillArray = (int[] arr )
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+    arr[i] = i + 1;
+}
+}
+
+int PrintArray = (int[] arr)
+{
+    foreach (var e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int GetSumOfElements = (int[] arr) 
+{
+    for ( i = 0; i < arr.Length; i++)
+    {
+    int sum = sum + arr[i];
+}
+}
+
+int GetProductOfElements = (int[] arr) 
+{
+    for ( i = 0; i < arr.Length; i++)
+    {
+    int product = product * arr[i];
+    }
+}
+
+
+int n = 10;
+int[] arr = new int[n];
+FillArray(arr);
+PrintArray(arr);
+int sum = GetSumOfElements(arr);
+int product = GetProductOfElements(arr);
+Console.WriteLine(sum);
+Console.WriteLine(product);
